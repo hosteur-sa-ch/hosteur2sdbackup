f494b8ebb484864fa96665aea7bbbef2c7edec49
FlorentHosteur <florent@hosteur.com>
2022-10-05 15:53:01 +0200

    fix chmod

--- hosteurbackup.sh
+++ hosteurbackup.sh
@@ -1,5 +1,6 @@
 cd /root/hosteur2sdbackup
 echo "Do script Update" >>/var/log/hosteurbackup.log
 git fetch && git stash && git pull  >>/var/log/hosteurbackup.log
+chmod +x ./hosteurbackup.sh
 source .restic_var
 sh hosteurbackup_dobackup.sh
