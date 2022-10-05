38a9c5c562069a85d8870514684cbd0fbe2fd510
FlorentHosteur <florent@hosteur.com>
2022-10-05 14:43:09 +0200

    main

--- hosteurbackup.sh
+++ hosteurbackup.sh
@@ -1,3 +1,2 @@
 cd /root/hosteur2sdbackup
-git fetch
-git pull  https://github.com/hosteur-sa-ch/hosteur2sdbackup.git
+git fetch && git stash && git pull  https://github.com/hosteur-sa-ch/hosteur2sdbackup.git
