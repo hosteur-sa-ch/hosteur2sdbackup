6eaf1bc81e4b53b997ee21e81e33d667f11e4316
FlorentHosteur <florent@hosteur.com>
2022-10-06 17:39:51 +0200

    change stats

--- hosteurbackup_stats.sh
+++ hosteurbackup_stats.sh
@@ -12,11 +12,4 @@ resticstats=`restic stats --json`
 size=$(echo "$resticstats" | jq '.total_size')
 num=$(echo "$resticstats" | jq '.total_file_count')
 host=`hostname`
-ip=`hostname -I | awk -F ' '  '{print $2;}'`
-curl -H 'Content-Type: application/json' -XPOST -u $RESTIC_OPS_LOGIN $RESTIC_OPS_URL/resticbkp_stats/_doc -d '{"hostname":"'$host'", "hostip":"'$ip'", "size":'$size', "nb_obj":'$num'}' 
-if [$host == "veeamrepo04-mrs2.hosteur.net"]; then
-  	repos09 = `ssh restic@10.128.0.86 "df -h | grep /dev/sda2" | awk -F ' ' '{print $5;}'`
-	repos08 = `ssh restic@10.128.0.90 "df -h | grep /dev/sda2" | awk -F ' ' '{print $5;}'`
-	curl -H 'Content-Type: application/json' -XPOST -u $RESTIC_OPS_LOGIN $RESTIC_OPS_URL/resticbkp_repos/_doc -d '{"hostname":"veeamrepo09-hostelyon", "used_percent":'$repos09'}'
-	curl -H 'Content-Type: application/json' -XPOST -u $RESTIC_OPS_LOGIN $RESTIC_OPS_URL/resticbkp_repos/_doc -d '{"hostname":"veeamrepo08-hostelyon", "used_percent":'$repos08'}'
-fi
+curl -H 'Content-Type: application/json' -XPOST -u $RESTIC_OPS_LOGIN $RESTIC_OPS_URL/resticbkp_stats/_doc -d '{"hostname":"'$host'", "repos":"'$RESTIC_BACKUPREPO'", "size":'$size', "nb_obj":'$num'}' 
