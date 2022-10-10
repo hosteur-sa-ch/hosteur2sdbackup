e136b77e12a4500fcc580171a1843b673452cfbf
FlorentHosteur <64596255+FlorentHosteur@users.noreply.github.com>
2022-10-10 17:53:46 +0200

    Update hosteurbackup.sh

--- hosteurbackup.sh
+++ hosteurbackup.sh
@@ -1,6 +1,6 @@
 cd /root/hosteur2sdbackup
 echo "Do script Update" >>/var/log/hosteurbackup.log
 git fetch && git stash && git pull  >>/var/log/hosteurbackup.log
-chmod +x ./hosteurbackup.sh
-source ./.restic_var
-sh hosteurbackup_dobackup.sh
+chmod +x /root/hosteur2sdbackup/hosteurbackup.sh
+source /root/hosteur2sdbackup/.restic_var
+sh /root/hosteur2sdbackup/hosteurbackup_dobackup.sh
