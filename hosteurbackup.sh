05dbd7e71f460e61e0af213e2d24e24cbb28a89c
FlorentHosteur <florent@hosteur.com>
2022-10-12 08:40:31 +0200

    change fix permission

--- hosteurbackup.sh
+++ hosteurbackup.sh
@@ -2,5 +2,7 @@ cd /root/hosteur2sdbackup
 echo "Do script Update" >>/var/log/hosteurbackup.log
 git fetch && git stash && git pull  >>/var/log/hosteurbackup.log
 chmod +x /root/hosteur2sdbackup/hosteurbackup.sh
+chmod +x /root/hosteur2sdbackup/hosteurbackup_stats.sh
+chmod +x /root/hosteur2sdbackup/hosteurbackup_dobackup.sh
 source /root/hosteur2sdbackup/.restic_var
-sh /root/hosteur2sdbackup/hosteurbackup_dobackup.sh
+/root/hosteur2sdbackup/hosteurbackup_dobackup.sh
