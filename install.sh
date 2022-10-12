aede9b93efddbd22c686f63fd381b0ae1ac7082d
FlorentHosteur <florent@hosteur.com>
2022-10-12 09:26:36 +0200

    completed install

--- install.sh
+++ install.sh
@@ -1,2 +1,9 @@
-cd /root/hosteur2sdbackup/
-/root/hosteur2sdbackup/hosteurbackup.sh
+cd /root/
+git clone https://github.com/hosteur-sa-ch/hosteur2sdbackup.git
+chmod +x /root/hosteur2sdbackup/hosteurbackup.sh
+chmod +x /root/hosteur2sdbackup/hosteurbackup_stats.sh
+chmod +x /root/hosteur2sdbackup/hosteurbackup_dobackup.sh
+yum install curl -y
+yum install jq -y
+apt install curl -y
+apt install jq -y
