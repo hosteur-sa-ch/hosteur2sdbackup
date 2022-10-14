2360685a6b9268bcc64af00c905478b053bc70cc
FlorentHosteur <florent@hosteur.com>
2022-10-14 09:58:24 +0200

    add update

--- hosteurbackup.sh
+++ hosteurbackup.sh
@@ -5,4 +5,6 @@ git fetch && git stash && git pull  >>/var/log/hosteurbackup.log
 chmod +x /root/hosteur2sdbackup/hosteurbackup.sh
 chmod +x /root/hosteur2sdbackup/hosteurbackup_stats.sh
 chmod +x /root/hosteur2sdbackup/hosteurbackup_dobackup.sh
+echo "Update restic" >>/var/log/hosteurbackup.log
+restic self-update
 /root/hosteur2sdbackup/hosteurbackup_dobackup.sh
