39a333d2ddfdc056d22897405f9416cfce82811e
FlorentHosteur <florent@hosteur.com>
2022-10-14 09:49:52 +0200

    fix source

--- hosteurbackup.sh
+++ hosteurbackup.sh
@@ -5,5 +5,4 @@ git fetch && git stash && git pull  >>/var/log/hosteurbackup.log
 chmod +x /root/hosteur2sdbackup/hosteurbackup.sh
 chmod +x /root/hosteur2sdbackup/hosteurbackup_stats.sh
 chmod +x /root/hosteur2sdbackup/hosteurbackup_dobackup.sh
-source /root/hosteur2sdbackup/.restic_var
 /root/hosteur2sdbackup/hosteurbackup_dobackup.sh
--- hosteurbackup_dobackup.sh
+++ hosteurbackup_dobackup.sh
@@ -5,8 +5,9 @@ if pgrep -x "$SERVICE" >/dev/null
 then
 	echo "backup already running" >>/var/log/hosteurbackup.log
 else
+    source /root/hosteur2sdbackup/.restic_var
 	restic backup --limit-upload $RESTIC_UPLOAD_LIMIT --tag Scheduled $RESTIC_BACKUPREPO >>/var/log/hosteurbackup.log 2>>/var/log/hosteurbackup_err.log
 	restic forget --keep-daily 7 --keep-monthly 1 --keep-yearly 1 --tag Scheduled >>/var/log/hosteurbackup.log 2>>/var/log/hosteurbackup_err.log
 	restic prune >>/var/log/hosteurbackup.log 2>>/var/log/hosteurbackup_err.log
-	sh hosteurbackup_stats.sh
+	/root/hosteur2sdbackup/hosteurbackup_stats.sh
 fi
