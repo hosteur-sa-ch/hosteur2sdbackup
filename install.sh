1580e9fa209195465849220e0a93a5d528c9f578
FlorentHosteur <florent@hosteur.com>
2022-10-14 09:58:17 +0200

    add auto update restic

--- install.sh
+++ install.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 cd /root/
+yum install curl -y
+yum install jq -y
+yum install restic -y
+yum install git -y
+apt install curl -y
+apt install jq -y
+apt install restic -y
+apt install git -y
+
 git clone https://github.com/hosteur-sa-ch/hosteur2sdbackup.git
 chmod +x /root/hosteur2sdbackup/hosteurbackup.sh
 chmod +x /root/hosteur2sdbackup/hosteurbackup_stats.sh
 chmod +x /root/hosteur2sdbackup/hosteurbackup_dobackup.sh
-yum install curl -y
-yum install jq -y
-yum install restic -y
-apt install curl -y
-apt install jq -y
-apr install restic -y
+
 
 source /root/hosteur2sdbackup/.restic_var
 restic init 
