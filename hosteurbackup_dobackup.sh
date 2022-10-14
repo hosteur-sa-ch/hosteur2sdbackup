9098c443b28bfca1dace2b8fe49cfc5cdcd03fdb
FlorentHosteur <florent@hosteur.com>
2022-10-14 10:07:29 +0200

    fix limit

--- hosteurbackup_dobackup.sh
+++ hosteurbackup_dobackup.sh
@@ -6,7 +6,7 @@ then
 	echo "backup already running" >>/var/log/hosteurbackup.log
 else
     source /root/hosteur2sdbackup/.restic_var
-	restic backup --limit-upload $RESTIC_UPLOAD_LIMIT --tag Scheduled $RESTIC_BACKUPREPO >>/var/log/hosteurbackup.log 2>>/var/log/hosteurbackup_err.log
+	restic backup --limit-upload 150000 --tag Scheduled $RESTIC_BACKUPREPO >>/var/log/hosteurbackup.log 2>>/var/log/hosteurbackup_err.log
 	restic forget --keep-daily 7 --keep-monthly 1 --keep-yearly 1 --tag Scheduled >>/var/log/hosteurbackup.log 2>>/var/log/hosteurbackup_err.log
 	restic prune >>/var/log/hosteurbackup.log 2>>/var/log/hosteurbackup_err.log
 	/root/hosteur2sdbackup/hosteurbackup_stats.sh
