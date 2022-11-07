a372582ab44d4ae99902a7558d3c28a813ab5efc
FlorentHosteur <florent@hosteur.com>
2022-11-07 09:29:41 +0100

    fix chmod unlock

--- hosteurbackup.sh
+++ hosteurbackup.sh
@@ -5,6 +5,7 @@ git fetch && git stash && git pull  >>/var/log/hosteurbackup.log
 chmod +x /root/hosteur2sdbackup/hosteurbackup.sh
 chmod +x /root/hosteur2sdbackup/hosteurbackup_stats.sh
 chmod +x /root/hosteur2sdbackup/hosteurbackup_dobackup.sh
+chmod +x /root/hosteur2sdbackup/hosteurbackup_unlock.sh
 echo "Update restic" >>/var/log/hosteurbackup.log
 restic self-update
 /root/hosteur2sdbackup/hosteurbackup_unlock.sh
