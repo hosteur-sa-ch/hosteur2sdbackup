22e6160899b4fce58e3db59cd65be09e7f93ca98
FlorentHosteur <64596255+FlorentHosteur@users.noreply.github.com>
2023-08-22 10:49:17 +0200

    Maintenance mode all 2sd job

--- hosteurbackup.sh
+++ hosteurbackup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd /root/hosteur2sdbackup
 echo "Do script Update" >>/var/log/hosteurbackup.log
+echo "Maintenance Mode"
+exit 0
 git fetch && git stash && git pull  >>/var/log/hosteurbackup.log
 chmod +x /root/hosteur2sdbackup/hosteurbackup.sh
 chmod +x /root/hosteur2sdbackup/hosteurbackup_stats.sh
